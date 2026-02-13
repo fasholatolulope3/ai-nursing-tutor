b748b264d326d11fd168499d838be55b30240861
Tolulope Fashola <fasholatolulope52@gmail.com>
2026-02-13 13:15:08 +0100

    fix: remove Laravel favicon references and update branding

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,9 +15,8 @@
 
     <title inertia>{{ config('app.name', 'Clinical Context') }}</title>
 
-    <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <!-- Branding Icons Removed to prevent Laravel logo from showing -->
+
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
